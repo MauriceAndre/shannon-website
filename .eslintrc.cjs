a1f392ff280cd370c4a3abf23b7ba06ea67c7d29
MauriceAndre <Maurice.Schmid@live.de>
2024-11-24 19:28:31 -0700

    feat: Set up Storybook with initial configuration and Spinner component stories

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,7 +23,7 @@ module.exports = {
   ignorePatterns: ["!**/.server", "!**/.client"],
 
   // Base config
-  extends: ["eslint:recommended"],
+  extends: ["eslint:recommended", "plugin:storybook/recommended"],
 
   overrides: [
     // React
