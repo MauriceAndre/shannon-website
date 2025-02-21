160a7836fd1d84f3adec0b70bbb4b4520eb7e98a
Maurice Schmid <msc@nwbc.de>
2025-02-20 20:58:36 -0700

    feat: Refactor Index page to use Fullpage component and enhance layout

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,12 +1,13 @@
-import { useRef } from "react";
 import type { MetaFunction } from "@remix-run/node";
 import styles from "./_index.module.scss";
+import { motion } from "motion/react";
 import PianoSvg from "~/components/icons/PianoSvg";
 import BookSvg from "~/components/icons/BookSvg";
 import Program from "~/components/Program/Program";
-import { IParallax, Parallax, ParallaxLayer } from "@react-spring/parallax";
 import FlipWords from "~/components/FlipWords/FlipWords";
 import { TextHighlight } from "~/components/HeroHighlight/TextHighlight";
+import Fullpage from "~/components/Fullpage/Fullpage";
+import Page from "~/components/Fullpage/Page";
 
 export const meta: MetaFunction = () => {
   return [
@@ -31,38 +32,23 @@ const getCurrentColor = () => {
 };
 
 export default function Index() {
-  const ref = useRef<IParallax>(null);
   const currentColor = getCurrentColor();
 
   return (
-    <Parallax pages={2} ref={ref}>
-      <ParallaxLayer offset={0} speed={0} style={{ zIndex: 0 }}>
-        <div className="flex flex-col items-center p-8 h-full relative">
-          <div className="flex justify-center items-center relative w-full h-full">
+    <Fullpage>
+      <Page title="Home" anchorName="home">
+        <div className="relative h-screen">
+          <div className="absolute inset-0 z-0 flex justify-center items-center">
             <img
               src="/background.png"
               alt="Calgary"
               className={`${styles["hero-image-background"]} absolute`}
             />
-          </div>
-        </div>
-      </ParallaxLayer>
-
-      <ParallaxLayer offset={0} speed={0.5} style={{ zIndex: 0 }}>
-        <div className="flex flex-col items-center p-8 h-full relative">
-          <div className="flex justify-center items-center relative w-full h-full">
             <img
               src="/clouds.png"
               alt="Calgary"
               className={`${styles["hero-image-clouds"]} absolute`}
             />
-          </div>
-        </div>
-      </ParallaxLayer>
-
-      <ParallaxLayer offset={0} speed={0} style={{ zIndex: 0 }}>
-        <div className="flex flex-col items-center p-8 h-full relative">
-          <div className="flex justify-center items-center relative w-full h-full">
             <img
               src="/tower.png"
               alt="Calgary"
@@ -71,25 +57,12 @@ export default function Index() {
                 filter: `drop-shadow(0 0 30px ${currentColor}) drop-shadow(0 0 60px ${currentColor})`,
               }}
             />
-          </div>
-        </div>
-      </ParallaxLayer>
-
-      <ParallaxLayer offset={0} speed={0} style={{ zIndex: 0 }}>
-        <div className="flex flex-col items-center p-8 h-full relative">
-          <div className="flex justify-center items-center relative w-full h-full">
             <img
               src="/calgary.png"
               alt="Calgary"
               className={`${styles["hero-image"]} absolute`}
             />
-          </div>
-        </div>
-      </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={0} style={{ zIndex: 1 }}>
-        <div className="flex flex-col items-center p-8 h-full relative">
-          <div className="flex justify-center items-center relative w-full h-full">
             <div className="absolute text-center text-white">
               <h1 className="text-6xl font-bold md:mb-4">
                 <span style={{ textShadow: "4px 4px 8px rgba(0, 0, 0, 0.7)" }}>
@@ -116,25 +89,31 @@ export default function Index() {
             </div>
           </div>
         </div>
-      </ParallaxLayer>
 
-      <ParallaxLayer offset={0.9} speed={3} style={{ zIndex: 2 }}>
-        <div className="flex flex-col items-center h-full relative">
-          <button
-            onClick={() => ref.current?.scrollTo(1)}
-            className="bg-white text-black p-2 rounded-full w-40 border-2 border-black z-10 flex flex-col items-center"
-          >
-            Pick a Class <br />
-            <img
+        <div className="absolute bottom-8 w-full flex flex-col items-center text-center">
+          <p className="text-white mb-2">Pick a Class</p>
+          <a href="#pick-a-class">
+            <motion.img
               src="/arrow-down.svg"
+              className="cursor-pointer invert"
               alt="Scroll down"
-              style={{ width: "20px", marginTop: "4px" }}
+              style={{ width: "40px" }}
+              whileHover={{ filter: "invert(1) drop-shadow(0 0 10px white)" }}
+              animate={{
+                y: [0, 10, 10, 10, 0],
+                transition: {
+                  repeat: Infinity,
+                  duration: 2,
+                  times: [0, 0.5, 0.5, 0.5, 1],
+                  repeatDelay: 1,
+                },
+              }}
             />
-          </button>
+          </a>
         </div>
-      </ParallaxLayer>
+      </Page>
 
-      <ParallaxLayer offset={1} speed={0}>
+      <Page title="Pick a Class" anchorName="pick-a-class">
         <div className="flex flex-col items-center content-center justify-around h-full">
           <h2 className="text-5xl font-bold mb-4">Pick a Class</h2>
           <div className="flex flex-col md:flex-row justify-around content-center w-full">
@@ -152,7 +131,64 @@ export default function Index() {
             />
           </div>
         </div>
-      </ParallaxLayer>
-    </Parallax>
+      </Page>
+
+      {/* <div className="absolute inset-0 z-1 flex flex-col items-center justify-center text-center text-white">
+        <h1 className="text-6xl font-bold md:mb-4">
+          <span style={{ textShadow: "4px 4px 8px rgba(0, 0, 0, 0.7)" }}>
+            <TextHighlight>
+              <FlipWords words={["Piano", "English"]} />
+            </TextHighlight>{" "}
+          </span>
+          <span
+            style={{
+              textShadow: "0 0 10px #000, 0 0 20px #000, 0 0 30px #000",
+            }}
+          >
+            Lessons
+          </span>
+        </h1>
+        <h2
+          className="text-6xl font-light"
+          style={{
+            textShadow: "0 0 10px #000, 0 0 20px #000, 0 0 30px #000",
+          }}
+        >
+          in the <span className="text-red-500">♥</span> of
+        </h2>
+      </div>
+      <div className="absolute bottom-10 w-full flex justify-center z-2">
+        <button
+          onClick={() =>
+            window.scrollTo({ top: window.innerHeight, behavior: "smooth" })
+          }
+          className="bg-white text-black p-2 rounded-full w-40 border-2 border-black flex flex-col items-center"
+        >
+          Pick a Class <br />
+          <img
+            src="/arrow-down.svg"
+            alt="Scroll down"
+            style={{ width: "20px", marginTop: "4px" }}
+          />
+        </button>
+      </div>
+      <div className="h-screen flex flex-col items-center content-center justify-around">
+        <h2 className="text-5xl font-bold mb-4">Pick a Class</h2>
+        <div className="flex flex-col md:flex-row justify-around content-center w-full">
+          <Program
+            className="w-full md:w-1/2 mb-4 md:mb-0"
+            title="Piano Lessons"
+            SvgComponent={PianoSvg}
+            url="piano"
+          />
+          <Program
+            className="w-full md:w-1/2"
+            title="English Tutoring"
+            SvgComponent={BookSvg}
+            url="english"
+          />
+        </div>
+      </div> */}
+    </Fullpage>
   );
 }
