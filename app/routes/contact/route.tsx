bc290167536380b68034af9839ec01b266d96d20
Maurice Schmid <msc@nwbc.de>
2025-02-26 19:03:17 -0700

    feat: Add Contact page with Fullpage layout and customizable Button component

---
+++ app/routes/contact/route.tsx
@@ -0,0 +1,16 @@
+﻿import Button from "~/components/Button/Button";
+import Fullpage from "~/components/Fullpage/Fullpage";
+import Page from "~/components/Fullpage/Page";
+
+export default function ContactPage() {
+  return (
+    <Fullpage>
+      <Page title="Contact" anchorName="contact">
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h1>Contact</h1>
+          <Button>Contact Me</Button>
+        </div>
+      </Page>
+    </Fullpage>
+  );
+}
